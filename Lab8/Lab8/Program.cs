4e1fb4f31c5e93d2a3d86ca7b38c2ce97fde4412
jarudat chaikuad <60030014@kmitl.ac.th>
2018-12-02 01:01:20 +0700

    1ok

---
+++ Lab8/Lab8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleAppArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = new int[10];
+            data[0] = 0;
+            data[1] = 1;
+            data[2] = 2;
+            data[3] = 3;
+            data[4] = 4;
+            data[5] = 5;
+            data[6] = 6;
+            data[7] = 7;
+            data[8] = 8;
+            data[9] = 9;
+            data[10] = 10;
+            Console.WriteLine(data[0]);
+            Console.WriteLine(data[1]);
+            Console.WriteLine(data[2]);
+            Console.WriteLine(data[3]);
+            Console.WriteLine(data[4]);
+            Console.WriteLine(data[5]);
+            Console.WriteLine(data[6]);
+            Console.WriteLine(data[7]);
+            Console.WriteLine(data[8]);
+            Console.WriteLine(data[9]);
+            Console.WriteLine(data[10]);
+            Console.ReadLine();
+        }
+    }
+}
